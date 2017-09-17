6f097943d738f87532aed967e143d8f2f8418147
VikaDinka <viktoria.gulin@gmail.com>
2017-09-17 23:08:38 +0300

    Helo world

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -10,6 +10,9 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
