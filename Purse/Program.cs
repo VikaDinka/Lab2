cc467a08027aa74a33f22dbba6ad382c22287c8d
VikaDinka <viktoria.gulin@gmail.com>
2017-09-17 21:12:52 +0300

    CreditCard commit

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -10,6 +10,10 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+            var money = new Money.Money(100);
+            Console.WriteLine(money.GetMoney());
+            var credit = new CreditCard.CreditCard(500);
+            Console.WriteLine(credit.GetSumCreditCard());
         }
     }
 }
